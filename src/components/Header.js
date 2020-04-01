7e940480e8fa6276d2e825a8834492427c996e35
Roy <roymeplease@gmail.com>
2020-04-01 19:16:05 +0300

    add the Header component with in img

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+            <img src="https://i.pinimg.com/originals/57/40/bb/5740bb4e1da387df4d92a09475c9b049.png" alt="):" className="maple" />
+        </div>
+    )
+}
+
+
+export default Header;
