2fa5f047eaef8dd77576e2cadb4c904c6cf61a24
Roy <roymeplease@gmail.com>
2020-04-03 02:31:43 +0300

    add 2 buttons for Login and Register

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,8 @@ function Header() {
         <div className="header">
             <img src="https://i.pinimg.com/originals/57/40/bb/5740bb4e1da387df4d92a09475c9b049.png" alt="):" />
             <h1>Todo-List</h1>
+            <button>Login</button>
+            <button>Register</button>
         </div>
     )
 }
