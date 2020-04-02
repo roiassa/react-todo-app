dc350afb6d5e9a8d4c8822dca2ff1c38fc4aefb9
Roy <roymeplease@gmail.com>
2020-04-03 02:23:32 +0300

    minor custimzation changes to the h1 in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Header() {
     return (
         <div className="header">
-            <img src="https://i.pinimg.com/originals/57/40/bb/5740bb4e1da387df4d92a09475c9b049.png" alt="):" className="maple" />
+            <img src="https://i.pinimg.com/originals/57/40/bb/5740bb4e1da387df4d92a09475c9b049.png" alt="):" />
             <h1>Todo-List</h1>
         </div>
     )
