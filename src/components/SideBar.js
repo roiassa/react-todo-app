5a65c47f3e5a2ab32680cd219a624b4c6fa6fe7c
Roy <roymeplease@gmail.com>
2020-04-03 02:00:19 +0300

    more customization to the side bar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 function SideBar() {
     return (
         <nav className="side-bar">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/" ><div className="link-div">
+                <FontAwesomeIcon icon={faHome} className="icon-home" />
+                <span className="nav-link">Home</span>
+            </div>
+            </Link>
+            <Link to="/about" ><div className="link-div">
+                <FontAwesomeIcon icon={faUserSecret} className="icon-user" />
+                <span className="nav-link">About</span>
+            </div>
+            </Link>
         </nav>
     )
 }
