06f996cb8fc1a20236bc89262fda8334262524f0
Roy <roymeplease@gmail.com>
2020-04-03 03:19:48 +0300

    add content to the Todo component and custimzation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Welcome from './components/Welcome'
 import About from './components/About'
 import SideBar from './components/SideBar'
+import Todo from './components/Todo'
 import './dist/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -17,6 +18,7 @@ function App() {
         <Header />
         <Welcome />
         <SideBar />
+        <Route exact path="/" component={Todo}></Route>
         <Route exact path="/about" component={Header, About}></Route>
       </React.Fragment>
     </Router>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 function Todo() {
-    
+    return (
+        <div className="todo-window">
+            <h1>Tasks</h1>
+            <div className="sections">
+                <button className="buttons">Tasks</button>
+                <button className="buttons">Done</button>
+            </div>
+            <div className="submit-div">
+                <input type="text" placeholder="Type your task here" />
+                <button type="submit">Submit</button>
+            </div>
+            <div className="tasks-div">
+
+            </div>
+        </div >
+    )
 }
 
 
