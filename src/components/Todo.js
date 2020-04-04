62f412c0c965d96126c4e15487e5356ccf53f330
Roy <roymeplease@gmail.com>
2020-04-04 22:42:01 +0300

    now when pressing the submit button to add a task, it adds a task to the todo-list div

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faBriefcase } from '@fortawesome/free-solid-svg-icons'
 
 
+
 function Todo() {
+    const [tasks, setTasks] = useState([])
+    const [input, setInput] = useState('')
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!input) {return;}
+        setTasks([...tasks, input])
+        console.log(input)
+        setInput('');
+    }
+
     return (
         <div className="todo-window">
             <h1>Tasks</h1>
@@ -19,16 +31,20 @@ function Todo() {
                         Done</button>
                 </div>
             </div>
-            <div className="submit-div">
-                <input type="text" placeholder="Type your task here" />
+            <form className="submit-form" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Type your task here" value={input} onChange={(e) => setInput(e.target.value)} />
                 <button type="submit">Submit</button>
-            </div>
-            <div className="tasks-div">
-
+            </form>
+            <div className="todo-list">
+                {tasks.map((task, index) => {
+                    return <div key={index}>
+                        <span >{task}</span>
+                        <button>Done</button>
+                        <button>Remove</button>
+                    </div>
+                })}
             </div>
         </div >
     )
 }
-
-
 export default Todo;
