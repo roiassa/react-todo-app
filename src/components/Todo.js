02c11f46daae1b21d24ef1b7208515f58097e39b
Roy <roymeplease@gmail.com>
2020-04-05 04:26:27 +0300

    toggling between the task list and the done list can happen now, still needs tweaking

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,35 +7,39 @@ import { faCheck, faBriefcase } from '@fortawesome/free-solid-svg-icons'
 function Todo() {
     const [tasks, setTasks] = useState([])
     const [input, setInput] = useState('')
-
+    const [taskList, setTaskList] = useState(null)
+    
     function handleSubmit(e) {
         e.preventDefault();
         if (!input) { return; }
         setTasks([...tasks, input])
-        console.log(input)
         setInput('');
     }
 
+
+   
+
     return (
         <div className="todo-window">
-            <h1>Tasks</h1>
+            {taskList ? <h1>Tasks</h1>: <h1>Done</h1>}
             <div className="sections">
                 <div>
-                    <button className="buttons">
+                    <button className="buttons" onClick={() => setTaskList(taskList === false)}>
                         <FontAwesomeIcon icon={faBriefcase} className="icon-briefcase" />
                         Tasks</button>
                 </div>
                 <div>
-                    <button className="buttons">
+                    <button className="buttons" onClick={() => setTaskList(taskList === false)}>
                         <FontAwesomeIcon icon={faCheck} className="icon-check" />
                         Done</button>
                 </div>
             </div>
+            
             <form className="submit-form" onSubmit={handleSubmit}>
                 <input type="text" placeholder="Type your task here" value={input} onChange={(e) => setInput(e.target.value)} />
                 <button type="submit">Submit</button>
             </form>
-            <div className="todo-list">
+            {taskList ? <div className="todo-list">
                 {tasks.map((task, index) => {
                     return <div className="todo-item" key={index}>
                         <span >{task}</span>
@@ -43,7 +47,10 @@ function Todo() {
                         <button>Remove</button>
                     </div>
                 })}
-            </div>
+            </div> : <div className="done-list">
+                <p style={{color : "white"}}>hi</p>
+            </div> }
+            
         </div >
     )
 }
