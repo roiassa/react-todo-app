7e37d825ac6d17feca83b7a40f417a7e5263e476
Roy <roymeplease@gmail.com>
2020-04-06 06:44:09 +0300

    error now pops up when the input field is empty

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,23 +5,41 @@ import { faCheck, faBriefcase } from '@fortawesome/free-solid-svg-icons'
 
 
 function Todo() {
-    const [tasks, setTasks] = useState([])
+    const [tasks, setTasks] = useState([{
+        value: null,
+        isDone: false,
+        toRemove: false
+    }])
     const [doneTasks, setDoneTasks] = useState([])
     const [input, setInput] = useState('')
     const [taskList, setTaskList] = useState(true)
+    const [error, setError] = useState(false)
 
     function handleSubmit(e) {
         e.preventDefault();
-        if (!input) { return; }
-        setTasks([...tasks, input])
-        setInput('');
+        if (!input) {
+            setError(true)
+        }
+        else {
+            setError(false)
+            setTasks([...tasks, tasks.value = input])
+            setInput('');
+            console.log(tasks.value);
+        }
     }
 
+
+    // function removeTask(arr, taskToRemove) {
+    //     const updatedTasks = arr.filter((task) => taskToRemove !== task);
+    //     setTasks(updatedTasks);
+    // }
 
     function removeTask(arr, taskToRemove) {
         const updatedTasks = arr.filter((task) => taskToRemove !== task);
         setTasks(updatedTasks);
     }
+
+
 
     function doneTask(doneArr, currArr, taskToDone) {
         setDoneTasks([...doneArr, taskToDone]);
@@ -43,24 +61,25 @@ function Todo() {
                         Done</button>
                 </div>
             </div>
-
+            {error && taskList ? <div className="error"><p>You have to type something in.</p> </div> : null}
             {taskList ? <form className="submit-form" onSubmit={handleSubmit}>
                 <input type="text" placeholder="Type your task here" value={input} onChange={(e) => setInput(e.target.value)} />
                 <button type="submit">Submit</button>
             </form> : null}
+            
             <div className="list-container">
                 {taskList ? <div className="task-list">
                     {tasks.map((task, index) => {
                         return <div className="todo-item" key={index}>
-                            <span>{task}</span>
+                            <span>{tasks.value}</span>
                             <button onClick={() => doneTask(doneTasks, tasks, task)}>Done</button>
                             <button onClick={() => removeTask(tasks, task)}>Remove</button>
                         </div>
                     })}
                 </div> : <div className="done-list">
                         {doneTasks.map((task, index) => {
-                            return <div className="todo-item" key={index}> 
-                                <span>{task}</span>
+                            return <div className="todo-item" key={index}>
+                                <span>{tasks.value}</span>
                                 <button onClick={() => removeTask(doneTasks, task)}>Remove</button>
                             </div>
                         })}
