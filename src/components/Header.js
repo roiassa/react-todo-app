5bf5f3457c2124dc7e5268770a224f50c85a083d
Roy <roymeplease@gmail.com>
2020-04-08 23:36:46 +0300

    new register window

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button'
 
 
 function Header() {
+    const [register, setRegister] = useState(false)
+
+
     return (
-        <div className="header">
-            <img src="https://i.pinimg.com/originals/57/40/bb/5740bb4e1da387df4d92a09475c9b049.png" alt="):" />
-            <h1>Todo-List</h1>
-            <button>Login</button>
-            <button>Register</button>
-        </div>
+        <React.Fragment>
+            <div className="header">
+                <img src="https://i.pinimg.com/originals/57/40/bb/5740bb4e1da387df4d92a09475c9b049.png" alt="):" />
+                <h1>Todo-List</h1>
+                <button>Login</button>
+                <button onClick={() => setRegister(true)}>Register</button>
+            </div>
+            {register ? <div className="register-wrapper">
+                <div className="register-window">
+                    <form className="register-form">
+                        <button className="close-btn">Close</button>
+                        <p>Your username:</p>
+                        <input type="text" />
+                        <p>Your password:</p>
+                        <input type="text" />
+                        <p>Repeat password:</p>
+                        <input type="text" />
+                        <p>Email:</p>
+                        <input type="text" />
+                        <Button as="input" type="submit" value="Submit" className="submit-btn"/>{' '}
+                    </form>
+                </div>
+            </div> : null}
+        </React.Fragment>
     )
 }
 
