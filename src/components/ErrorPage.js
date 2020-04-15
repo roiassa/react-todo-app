1847a986c62d19111ca4a9f498809abb475033c9
Roy <roymeplease@gmail.com>
2020-04-15 05:31:30 +0300

    new Error Page component

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ErrorPage() {
+    return (
+        <div className="error-age">
+            <h1>Oops... 404: Page not found</h1>
+        </div>
+    )
+}
+
+export default ErrorPage
--- src/components/todo-window/Todos.js
+++ src/components/todo-window/Todos.js
@@ -2,46 +2,57 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TodoItem from "./TodoItem"
 import Sections from "./Sections"
+import ErrorPage from "../ErrorPage"
 
 
 function Todos() {
     const [tasks, setTasks] = useState([])
     const [taskList, setTaskList] = useState(true)
+    const [error, setError] = useState(false)
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/todos`)
+        axios.get(`http://localhost:5000/todoss`)
             .then(res => {
                 setTasks(res.data)
+            })
+            .catch(err => {
+                if (err.response.status === 404) {
+                    setError(true);
+                }
             })
     }, [])
 
     return (
-        <div className="todo-window">>
-            <Sections setTasks={setTasks} tasks={tasks} taskList={taskList} setTaskList={setTaskList} />
-            <div className="list-container">
-                {taskList ? <div className="task-list">
-                    {tasks.map((task, index) => {
-                        return <TodoItem
-                            tasks={tasks}
-                            setTasks={setTasks}
-                            index={index}
-                            task={task} />
-                    })}
-                </div>
-                    :
-                    <div className="done-list">
-                        {tasks.map((task, index) => {
-                            return <TodoItem
-                                doneFlag={!taskList}
-                                tasks={tasks}
-                                setTasks={setTasks}
-                                index={index}
-                                task={task} />
-                        })}
+        <React.Fragment>
+            {error ? <ErrorPage />
+                :
+                <div className="todo-window">
+                    <Sections setTasks={setTasks} tasks={tasks} taskList={taskList} setTaskList={setTaskList} />
+                    <div className="list-container">
+                        {taskList ? <div className="task-list">
+                            {tasks.map((task, index) => {
+                                return <TodoItem
+                                    tasks={tasks}
+                                    setTasks={setTasks}
+                                    index={index}
+                                    task={task} />
+                            })}
+                        </div>
+                            :
+                            <div className="done-list">
+                                {tasks.map((task, index) => {
+                                    return <TodoItem
+                                        doneFlag={!taskList}
+                                        tasks={tasks}
+                                        setTasks={setTasks}
+                                        index={index}
+                                        task={task} />
+                                })}
+                            </div>
+                        }
                     </div>
-                }
-            </div>
-        </div>
+                </div>}
+        </React.Fragment>
     )
 }
 
